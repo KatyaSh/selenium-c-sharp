e98213323fa99f7f0ccac1f66f22ec776209e232
Katsiaryna Sharstuk <Katsiaryna.Sharstuk@ventionteams.com>
2025-07-30 18:30:38 +0200

    HomeTask module 3 is done

---
+++ selenium/LogoutTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+public class LogoutTests : BaseTest
+{
+    [Test]
+    public void Logout()
+    {
+        SuccesfullLogin("standard_user", "secret_sauce");
+        var burgerMenuButton = driver.FindElement(By.Id("react-burger-menu-btn"));
+        burgerMenuButton.Click();
+        //var logoutButton = WaitForElement(By.Id("logout_sidebar_link"));
+        var logoutButton = webDriverWait.Until(
+            SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("logout_sidebar_link"))
+        );
+        logoutButton.Click();
+        var actualUrl = driver.Url;
+        var expectedUrl = "https://www.saucedemo.com/";
+
+        Assert.That(actualUrl, Is.EqualTo(expectedUrl), "Wrong URL for Login page");
+    }
+}
+
